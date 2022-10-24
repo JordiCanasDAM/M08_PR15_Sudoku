3ced1f5f011223d633d470d2823c3a2486029e0e
Jordi <jcanasalvarez.cf@iesesteveterradas.cat>
2022-10-24 16:07:42 +0200

    Taulell funcional versió generació linia a linia

--- app/src/main/java/com/example/m08_pr15_sudoku/MainActivity.java
+++ app/src/main/java/com/example/m08_pr15_sudoku/MainActivity.java
@@ -12,6 +12,8 @@ import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -23,51 +25,44 @@ public class MainActivity extends AppCompatActivity {
         String[] nombres = {"▣","1","2","3","4","5","6","7","8","9"};
 
         //Creacio taulell
+
         TableLayout table = findViewById(R.id.table);
-        boolean squareColor = true;
-        //Files de quadrats
-        for (int a = 0; a<3 ; a++){
-            TableRow squareRow = new TableRow(this);
-            //Quadrats
-            for (int b = 0; b<3; b++){
-                TableLayout square = new TableLayout(this);
-                if (squareColor){
-                    square.setBackgroundColor(Color.LTGRAY);
-                    squareColor = false;
-                } else {
-                    square.setBackgroundColor(Color.WHITE);
-                    squareColor = true;
-                }
-                //Files de numeros
-                for (int c = 0; c<3 ; c++){
-                    TableRow numRow = new TableRow(this);
-                    //Numeros
-                    for (int d = 0; d<3; d++){
-                        //Spinner
-                        Spinner spinner = new Spinner(this);
-                        //Aspecte spinner
-                        spinner.setBackground(null);
-                        spinner.setPadding(15, 15, 15, 15);
-                        //Valors spinner
-                        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item, nombres);
-                        spinner.setAdapter(adapter);
-                        //Listener spinner
-                        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                            @Override public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                                // la posició del spinner és 'i', però també es pot buscar amb
-                                String string = spinner.getSelectedItem().toString();
-                                Log.i("INFO","Spinner modificat");
-                            }
-                            @Override public void onNothingSelected(AdapterView<?> adapterView) {}
-                        });
-                        //Insercio spinner
-                        numRow.addView(spinner);
+        //Files
+        for (int a = 0; a<9; a++){
+            TableRow row = new TableRow(this);
+            for (int b = 0; b<9; b++){
+                //Spinner
+                Spinner spinner = new Spinner(this);
+                //Aspecte spinner
+                if ((a > 2 && a < 6 || b > 2 && b < 6)) { //Si es centre de fila o columna
+                    if (a > 2 && a < 6 && b > 2 && b < 6) { //Si es centre de taulell
+                        spinner.setBackgroundColor(Color.LTGRAY);
+                    } else {
+                        spinner.setBackgroundColor(Color.WHITE);
                     }
-                    square.addView(numRow);
+                } else { //Si es cantonada
+                    spinner.setBackgroundColor(Color.LTGRAY);
                 }
-                squareRow.addView(square);
+                spinner.setPadding(15, 15, 15, 15);
+                //Valors spinner
+                ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item, nombres);
+                spinner.setAdapter(adapter);
+                //Listener spinner
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        // la posició del spinner és 'i', però també es pot buscar amb
+                        String string = spinner.getSelectedItem().toString();
+                        Log.i("INFO","Spinner modificat");
+                    }
+                    @Override public void onNothingSelected(AdapterView<?> adapterView) {} //Ignore
+                });
+                //Tags spinner
+                //spinner.setTag(R.id.fila,fila);
+                //spinner.setTag(R.id.col,col);
+                //Insercio spinner
+                row.addView(spinner);
             }
-            table.addView(squareRow);
+            table.addView(row);
         }//Fi creacio taulell
 
 
